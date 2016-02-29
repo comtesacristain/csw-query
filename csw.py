d063f3fea7bc81143b4488f49a65e2af7ab5e48c
Michael Sexton <mick.sexton@gmail.com>
2016-02-29 15:30:32 +1100

    Basic loop with sleep

--- csw.py
+++ csw.py
@@ -1,12 +1,20 @@
 # hit CSW until it says there are no records
+import datetime
+import time
+import urllib
+import xml.dom.minidom as minidom
 
-from bs4 import BeautifulSoup
-import urllib3
-
-urllib3.disable_warnings()
-http_proxy = os.environ.get('http_proxy')
-http = urllib3.proxy_from_url(http_proxy) if http_proxy else urllib3.PoolManager()
-
-proxies = {"http":"http://pc-65024:5865"}
 csw_url="http://dev.portal.geoscience.gov.au/geonetwork/srv/eng/csw-australian-topography-featured?SERVICE=CSW&REQUEST=GetRecords&version=2.0.2&resultType=results&maxrecords=50"
-csw = BeautifulSoup(urllib.urlopen(csw_url,proxies=proxies))
+
+num_results = 1
+
+while num_results != 0:
+    response = urllib.urlopen(csw_url)
+    csw_doc = response.read()
+    xml_response = minidom.parseString(csw_doc)
+    search_results=xml_response.getElementsByTagName("csw:SearchResults")[0]
+    num_results=int(search_results.getAttribute("numberOfRecordsMatched"))
+    print num_results
+    print time.asctime(time.localtime(time.time()))
+    #print datetime.datetime(time.localtime(time.time()))
+    time.sleep(300)
